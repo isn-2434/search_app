cd71b9ed043e1a1a781d70c50af7ce5a166cbac0
isn-2434 <isshin.hamamoto0814@gmail.com>
2025-02-10 17:47:35 +0900

    選手登録設定

--- search_app/models.py
+++ search_app/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+POSITION = [('ミドルブロッカー','MB'),('ウイングスパイカー','WS'),('セッター','S'),('リベロ','L'),('オポジット','OP'),('アウトサイドヒッター','OH'),('マネージャー','MG'),('監督','AD'),('コーチ','CH')]
+    
+class Product(models.Model): 
+    id = models.BigAutoField(primary_key=True) 
+    name = models.CharField(max_length=255)
+    age = models.IntegerField(default=1)
+    height = models.DecimalField(max_digits=5, decimal_places=1,default=1)
+    weight = models.DecimalField(max_digits=5, decimal_places=1,default=1)
+    team = models.CharField(max_length=255,default="烏野高校")
+    position = models.CharField(max_length=255,choices=POSITION)
+    description = models.TextField(default="---")
+    image = models.ImageField(default='../static/image/noimg.jpg')
+
+    def __str__(self): 
+        
+        return self.name
--- search_app/urls.py
+++ search_app/urls.py
@@ -1,2 +1,13 @@
 from django.urls import path 
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path('', views.search_view,name='search_view'),
+    path('product/new/', views.product_create, name='product_create'),
+    path('product/<int:pk>/', views.product_detail, name='product_detail'), 
+    path('product/<int:pk>/edit/',  views.product_update, name='product_update'), 
+    path('product/<int:pk>/delete',  views.product_delete, name='product_delete'), 
+    path('product/', views.product_list, name='product_list'),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
